0a357a5a8cab8261048ea858c3d3ca98f317ec08
rukshar69 <ruksharalam7@gmail.com>
2020-09-21 16:02:00 +0600

    game added

---
+++ choice-game/src/CharacterCard.js
@@ -0,0 +1,32 @@
+import React from "react";
+
+function CharacterCard(props) {
+  return (
+    <div
+      className={"card " + (props.Clicked ? "animate" : "")}
+      onClick={() => props.handleClick(props.id)}
+    >
+      <div className="img-container">
+        <img alt={props.name} src={props.image} />
+      </div>
+      <div className="img-content">
+        <ul>
+          <li>
+            <strong>Name:</strong>
+            {props.name}
+          </li>
+          <li>
+            <strong>Occupation:</strong>
+            {props.occupation}
+          </li>
+          <li>
+            <strong>Nationality:</strong>
+            {props.nationality}
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default CharacterCard;
